18c653ce62531c7cf6099f38695790a98608fafd
Natanael <NatanaelUF17@gmail.com>
2023-03-07 12:03:08 -0400

    Uploading project with some progress

---
+++ WaterMonitoring/WaterMonitoring/AssemblyInfo.cs
@@ -0,0 +1,7 @@
+﻿using Xamarin.Forms.Xaml;
+using Xamarin.Forms;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+[assembly: ExportFont("Montserrat-Bold.ttf", Alias = "Montserrat-Bold")]
+[assembly: ExportFont("Montserrat-SemiBold.ttf", Alias = "Montserrat-SemiBold")]
+[assembly: ExportFont("Montserrat-Regular.ttf", Alias = "Montserrat-Regular")]
